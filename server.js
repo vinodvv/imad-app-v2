0ccc47aa7c21da7d4f76c88dd894c61e19575267
vinodvv <vinovijayan@gmail.com>
2017-07-13 00:00:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,14 +95,6 @@ app.get('/:articleName', function(req,res){
     res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
-
 app.get('/article-four', function(req,res){
     res.send('Article four requested and will be server here');
 });
