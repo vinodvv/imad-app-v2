7384a385c9f0c2ea15bb9cd195a387a0aa88ba35
vinodvv <vinovijayan@gmail.com>
2017-07-12 22:05:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/arcticle-one', function(req,res){
+    res.send('Arcticle one requested and will be server here');
+});
+
+app.get('/arcticle-two', function(req,res){
+    res.send('Arcticle two requested and will be server here');
+});
+
+app.get('/arcticle-three', function(req,res){
+    res.send('Arcticle three requested and will be server here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
